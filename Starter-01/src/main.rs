be3ab3f1caceddcc0cdabb568e2911a35987337f
weiminw <weimin.wan@deepgram.com>
2023-08-07 14:57:38 -0400

    cleanup code

--- Starter-01/src/main.rs
+++ Starter-01/src/main.rs
@@ -1,26 +1,22 @@
 #[macro_use] extern crate rocket;
 
-use rocket::{Rocket, Build};
-use rocket::fairing::AdHoc;
-use rocket::request::FlashMessage;
 use rocket::serde::{Serialize, Deserialize};
 use rocket::serde::json::Json;
-use serde_json::Value;
 use rocket::form::Form;
-use rocket::config::{Config};
+use rocket::config::Config;
 use rocket::fs::{FileServer, relative, TempFile};
-use reqwest::{Client, Error};
+use reqwest::Client;
 use std::env;
 use dotenv::dotenv;
 
 #[derive(FromForm)]
 struct Request<'f> {
     url: Option<String>,
-    features: Option<String>,
+    file: Option<TempFile<'f>>,
     model: Option<String>,
     version: Option<String>,
     tier: Option<String>,
-    file: Option<TempFile<'f>>,
+    features: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -36,13 +32,13 @@ struct Response {
 async fn transcribe(request: Form<Request<'_>>) ->Json<Response> {
     dotenv().ok();
     let api_key = env::var("deepgram_api_key").expect("deepgram api_key is not set");
-
+    
     let url = request.url.clone().unwrap_or_default();
     let model = request.model.clone().unwrap_or_default();
     let version = request.version.clone().unwrap_or_default();
     let tier = request.tier.clone().unwrap_or_default();
-
     let features = request.features.clone().unwrap_or_default();
+
     let mut dg_features: serde_json::Value = serde_json::from_str(&features).expect("Failed to parse features");
 
     dg_features["model"] = serde_json::Value::String(model.clone());
@@ -55,12 +51,17 @@ async fn transcribe(request: Form<Request<'_>>) ->Json<Response> {
         dg_features["tier"] = serde_json::Value::String(tier.clone());
     }
 
+    let mut body_data = serde_json::json!({});
 
-    let body_data = serde_json::json!({
-        "url": url
-    });
-
-    let client = reqwest::Client::new();
+    if url.starts_with("https://res.cloudinary.com/deepgram") {
+        body_data = serde_json::json!({
+            "url": url
+        });
+    } else if let Some(file) = &request.file {
+        // TODO: fix file upload
+    }
+    
+    let client = Client::new();
     let endpoint = "https://api.deepgram.com/v1/listen";
     let query_str = serde_urlencoded::to_string(&dg_features).expect("Failed to encode url");
     let response = client.post(format!("{}?{}", endpoint, query_str))
@@ -68,8 +69,8 @@ async fn transcribe(request: Form<Request<'_>>) ->Json<Response> {
         .json(&body_data)
         .send()
         .await;
-
-    let transcription: serde_json::Value = response.unwrap().json().await.expect("Failed to parse JSON");
+    
+    let transcription: serde_json::Value = response.unwrap().json().await.expect("Failed to parse JSON from Deepgram");
     let res_data = Response {
         model: Some(model),
         version: Some(version),
@@ -78,7 +79,6 @@ async fn transcribe(request: Form<Request<'_>>) ->Json<Response> {
         transcription: transcription,
     };
                    
-    println!("{:?}", features);
     Json(res_data)
 }
         
@@ -91,7 +91,7 @@ fn rocket() -> _ {
     .parse::<u16>()
     .expect("Invalid port number");
 
-    let figment = rocket::Config::figment()
+    let figment = Config::figment()
     .merge(("port", port));
 
     rocket::custom(figment)
